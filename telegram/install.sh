a9b825bef90a12f4930b58eacc10e2c2b3cd0ee2
Your Name <you@example.com>
2025-05-30 18:46:04 +0300

    xwq#	изменено:      telegram/install.sh

--- telegram/install.sh
+++ telegram/install.sh
@@ -3,5 +3,5 @@ cd /tmp
 wget https://github.com/definitly486/definitly486/releases/download/telegramdesktop/TelegramDesktop.tar.xz.enc
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in TelegramDesktop.tar.xz.enc -out TelegramDesktop.tar.xz
 rm -R $HOME/.local/share/TelegramDesktop/tdata
+tar -xJf TelegramDesktop.tar.xz
 cp -R TelegramDesktop/tdata  $HOME/.local/share/TelegramDesktop/
-tar -xJf TelegramDesktop.tar.xz 
