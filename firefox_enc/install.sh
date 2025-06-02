63fd6a6e8aad0f60a5c8237b6f3dca09bfcc2689
Your Name <you@example.com>
2025-06-02 23:31:33 +0300

    изменено:      firefox_enc/install.sh

--- firefox_enc/install.sh
+++ firefox_enc/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+cd /tmp 
 wget https://github.com/definitly486/definitly486/releases/download/firefox.enc/firefox.tar.xz.enc
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in  firefox.tar.xz.enc  -out firefox.tar.xz
 tar -xJf firefox.tar.xz
+rm -R $HOME/.mozilla
+mkdir $HOME/.mozilla
+cp -R firefox  $HOME/.mozilla
