382dc195b404eb568517926d7769ac56bb4fc5af
Your Name <you@example.com>
2025-06-03 23:15:45 +0300

    изменено:      firefox_enc/install.sh 	изменено:      telegram/install.sh

--- firefox_enc/install.sh
+++ firefox_enc/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 cd /tmp 
-wget https://github.com/definitly486/definitly486/releases/download/firefox.enc/firefox.tar.xz.enc
+
+if ! [  -f "/tmp/firefox.tar.xz.enc" ]; then
+    wget https://github.com/definitly486/definitly486/releases/download/firefox.enc/firefox.tar.xz.enc
+fi
+
+
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in  firefox.tar.xz.enc  -out firefox.tar.xz
 tar -xJf firefox.tar.xz
 rm -R $HOME/.mozilla
--- telegram/install.sh
+++ telegram/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd /tmp 
-wget https://github.com/definitly486/definitly486/releases/download/telegramdesktop/TelegramDesktop.tar.xz.enc
+
+if ! [  -f "/tmp/TelegramDesktop.tar.xz.enc" ]; then
+      wget https://github.com/definitly486/definitly486/releases/download/telegramdesktop/TelegramDesktop.tar.xz.enc
+fi
+
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in TelegramDesktop.tar.xz.enc -out TelegramDesktop.tar.xz
 rm -R $HOME/.local/share/TelegramDesktop/tdata
 tar -xJf TelegramDesktop.tar.xz
