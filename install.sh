27366a467da2f564ba6ad13a498292657dda0c85
Your Name <you@example.com>
2025-06-04 12:19:22 +0300

    новый файл:    install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+install_apt () {
+
+ apt install git mc screen htop sshpass 
+
+}
+
+install_dnf  () {
+
+ dnf  install git mc screen htop sshpass openssl 
+
+}
+
+if [ "debian" = "$(uname -n)"   ] ; then
+    
+    echo "install packages for debian "
+    install_apt
+else
+    echo "install packages for fedora"
+    install_dnf
+fi
