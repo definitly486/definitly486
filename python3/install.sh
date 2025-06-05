1548a99b6bde1acd946b99a0c9b24e3a10c50da6
Your Name <you@example.com>
2025-06-05 16:51:28 +0300

    новый файл:    python3/Setup 	изменено:      python3/install.sh

--- python3/install.sh
+++ python3/install.sh
@@ -6,6 +6,9 @@ NAME=Python-3.9.0b4
 VER=$(echo $NAME | sed 's/.......//' |  sed 's/^\(.*\).$/\1/' | sed 's/^\(.*\).$/\1/')
 wget https://www.python.org/ftp/python/$VER/$NAME.tar.xz
 tar xf $NAME.tar.xz
+
+cp Setup $NAME/Modules
+
 cd $NAME
 ./configure
 make && sudo make install
