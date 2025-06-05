2337c32d99e45a589eaf13221280f549a80a16d3
Your Name <you@example.com>
2025-06-05 10:13:01 +0300

    новый файл:    sshpass/install_sshpass.sh

---
+++ sshpass/install_sshpass.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget https://sources.voidlinux.org/sshpass-1.10/sshpass-1.10.tar.gz
+tar -xf sshpass-1.10.tar.gz
+cd sshpass-1.10
+./configure
+sudo make install
+sudo cp /usr/local/bin/sshpass /usr/bin
