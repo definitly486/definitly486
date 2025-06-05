3185c17ee24be2a6b485bfb1ea26fba4234eca94
Your Name <you@example.com>
2025-06-05 16:28:43 +0300

    изменено:      python3/install.sh

--- python3/install.sh
+++ python3/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt-get install -y  zlib1g-dev
+
 NAME=Python-3.9.0b4
 VER=$(echo $NAME | sed 's/.......//' |  sed 's/^\(.*\).$/\1/' | sed 's/^\(.*\).$/\1/')
 wget https://www.python.org/ftp/python/$VER/$NAME.tar.xz
