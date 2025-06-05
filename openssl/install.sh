eac5ea8721b85d8d03ba401a17e484dab58a8447
Your Name <you@example.com>
2025-06-05 20:03:10 +0300

    изменено:      openssl/install.sh

--- openssl/install.sh
+++ openssl/install.sh
@@ -4,8 +4,8 @@ wget https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1w/openssl
 tar xf openssl-1.1.1w.tar.gz
 
 cd openssl-1.1.1w
+./config
+make && sudo make install
 
-
-
-sudo cp /usr/local/lib64/libcrypto.so.1.1 /usr/lib
-sudo cp /usr/local/lib64/libssl.so.1.1 /usr/lib
+sudo cp /usr/local/lib/libssl.so.1.1 /usr/lib
+sudo cp /usr/local/lib/libcrypto.so.1.1  /usr/lib
