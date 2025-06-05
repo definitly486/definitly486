bab989a446263e4266599cedd9123294c9b9ff96
Your Name <you@example.com>
2025-06-05 16:20:17 +0300

    изменено:      python3/install.sh

--- python3/install.sh
+++ python3/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 NAME=Python-3.9.0b4
-
-wget https://www.python.org/ftp/python/3.11.0/$NAME.tar.xz
+VER=$(echo $NAME | sed 's/.......//' |  sed 's/^\(.*\).$/\1/' | sed 's/^\(.*\).$/\1/')
+wget https://www.python.org/ftp/python/$VER/$NAME.tar.xz
 tar xf $NAME.tar.xz
 cd $NAME
 ./configure
