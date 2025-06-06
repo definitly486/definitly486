336c16e1ec977f63ec63dd9b35f59d8ec7904040
Your Name <you@example.com>
2025-06-06 17:34:05 +0300

    изменено:                   install_firefox.sh

--- install_firefox.sh
+++ install_firefox.sh
@@ -5,7 +5,7 @@ ver=139.0.1
 cd /tmp
 
 if ! [  -f "/tmp/firefox-$ver.tar.xz" ]; then
-     wget https://ftp.mozilla.org/pub/firefox/releases/139.0.1/linux-x86_64/ru/firefox-$ver.tar.xz
+     wget --no-check-certificate https://ftp.mozilla.org/pub/firefox/releases/139.0.1/linux-x86_64/ru/firefox-$ver.tar.xz
 fi
 
 tar -xf firefox-$ver.tar.xz -C $HOME
