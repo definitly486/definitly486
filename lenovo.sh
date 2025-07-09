e3c164a598e9b014d490f3ceaf1e2afeb0dd5824
definitly486 <fg8989@gmail.com>
2025-07-09 15:33:06 +0300

    Update lenovo.sh

--- lenovo.sh
+++ lenovo.sh
@@ -5,3 +5,5 @@ DIR=$(dirname "$(realpath $0)")
 curl -L -o main.tar.gz https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/archive/main.tar.gz
 tar -xf main.tar.gz
 adb shell pm grant com.conena.navigation.gesture.control android.permission.WRITE_SECURE_SETTINGS 
+
+https://raw.githubusercontent.com/Syavar/V2ray-Configs/refs/heads/main/OK_TLS_telegram.org_base64.txt
