71a51a885dd3bc16e43f77431d75ac1c9b9cd02c
Your Name <you@example.com>
2025-07-09 15:56:42 +0300

    новый файл:    googleauth/install.sh

---
+++ googleauth/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /tmp 
+
+if ! [  -f "/tmp/authenticator.txt.enc" ]; then
+    wget https://github.com/definitly486/definitly486/releases/download/googleauth/authenticator.txt.enc
+fi
+
+
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in  authenticator.txt.enc  -out authenticator.txt
