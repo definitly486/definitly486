fec7222238ea0e2d1f568c9566f42434293e8f25
definitly486 <fg8989@gmail.com>
2025-07-11 20:21:01 +0300

    Update lenovo.sh

--- lenovo.sh
+++ lenovo.sh
@@ -3,8 +3,8 @@ termux-setup-storage
 DIR=$(dirname "$(realpath $0)")
 
 cd $HOME
-curl -L -o $TERMUX_PREFIX/tmp/main.tar.gz https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/archive/main.tar.gz
-tar -xf $TERMUX_PREFIX/tmp/main.tar.gz -С $HOME
+curl -L -o $TERMUX__PREFIX/tmp/main.tar.gz https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/archive/main.tar.gz
+tar -xf $TERMUX__PREFIX/tmp/main.tar.gz -С $HOME
 adb shell pm grant com.conena.navigation.gesture.control android.permission.WRITE_SECURE_SETTINGS 
 
 https://raw.githubusercontent.com/Syavar/V2ray-Configs/refs/heads/main/OK_TLS_telegram.org_base64.txt
