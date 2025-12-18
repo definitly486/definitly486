ef7741210cf8ee544a04bf539f918338a7f8744c
Your Name <you@example.com>
2025-12-18 12:32:24 +0300

    изменено:      monitor.sh 	изменено:      virtio-9p

--- monitor.sh
+++ monitor.sh
@@ -6,8 +6,8 @@ do
 
 cputemp=$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)
 top=$(top | awk 'NR==9;' | awk '{print $12 ,$11}')
-echo cputemp:$cputemp
-echo top:$top
+echo cputemp:$cputemp >> $HOME/log
+echo top:$top >> $HOME/log
 
-sleep 10
+sleep 60
 done
