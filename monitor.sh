9cffcfe3c054d4e9f89c21ead7068650551fc1bf
Your Name <you@example.com>
2026-03-18 17:52:50 +0300

    изменено:      monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+sshpass -p  639639 ssh definitly@192.168.8.103 << 'EOF'
+#!/bin/sh
 
 var1=5
 while [ $var1 -gt 0 ]
 do
+  cputemp=$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)
+  top=$(top | awk 'NR==9' | awk '{print $12 ,$11}')
 
-cputemp=$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)
-top=$(top | awk 'NR==9;' | awk '{print $12 ,$11}')
-echo cputemp:$cputemp >> $HOME/log
-echo top:$top >> $HOME/log
+  echo cputemp:$cputemp >> $HOME/log
+  echo top:$top >> $HOME/log
 
-sleep 60
+  sleep 60
 done
+EOF
