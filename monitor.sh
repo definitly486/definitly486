b57259f5beb7e4a1325fe2381b040e2cc7e2d52b
definitly486 <fg8989@gmail.com>
2026-03-20 06:40:09 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
-sshpass -p  639639 ssh definitly@192.168.8.103 << 'EOF'
+sshpass -p  639639 ssh  -T definitly@192.168.8.103 << 'EOF'
 #!/bin/sh
+
+exec >> "$HOME/log" 2>&1
+
+LOCKFILE="/tmp/myscript.lock"
+
+if [ -f "$LOCKFILE" ]; then
+  echo "Already running"
+  exit 1
+fi
+
+trap "rm -f $LOCKFILE" EXIT
+touch "$LOCKFILE"
 
 var1=5
 while [ $var1 -gt 0 ]
 do
   cputemp=$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)
-  top=$(ps -axo comm,%cpu | sort -k2 -nr | awk 'NR==2 {print $1, $2}')
+  top_proc=$(ps -axo comm,%cpu | sort -k2 -nr | awk 'NR==2 {print $1, $2}')
   now=$(date "+%Y-%m-%d %H:%M:%S")
 
-  echo "time:$now cputemp:$cputemp" >> $HOME/log
-  echo "time:$now top:$top" >> $HOME/log
+  echo "PID:$$ time:$now cputemp:$cputemp"
+  echo "time:$now top:$top_proc"
 
   sleep 60
+  var1=$((var1 - 1))
 done
 EOF
